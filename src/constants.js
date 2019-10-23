42e0921d8bfbf742cec14293a563554c8a439327
Alex Pate <alexpate@monzo.com>
2019-10-23 10:20:13 +0100

    Allow Preview to be used as a page name

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,8 @@
 export const SCOPED_PAGE_NAMES = ['iOS', 'Android', 'Master'];
-export const ALLOWED_PAGE_NAMES = ['iOS', 'Android', 'Master', 'Symbols'];
+export const ALLOWED_PAGE_NAMES = [
+  'iOS',
+  'Android',
+  'Master',
+  'Symbols',
+  'Preview',
+];
